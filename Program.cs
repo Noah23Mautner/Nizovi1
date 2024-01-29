351e1add437ff987bf6f958ac9fd33c3869a6187
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-29 16:50:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nizovi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neparniNiz = new int[10];
+            int[] parniNiz = new int[10];
+            int neparniBrojac = 0;
+            int parniBrojac = 0;
+
+            while (true)
+            {
+                Console.Write("Unesite broj: ");
+                int broj = int.Parse(Console.ReadLine());
+
+                if (broj == 0 || neparniBrojac + parniBrojac >= 10)
+                {
+                    break;
+                }
+
+                if (broj % 2 == 0)
+                {
+                    parniNiz[parniBrojac] = broj;
+                    parniBrojac++;
+                }
+                else
+                {
+                    neparniNiz[neparniBrojac] = broj;
+                    neparniBrojac++;
+                }
+            }
+
+            Console.WriteLine("Neparni brojevi:");
+            for (int i = 0; i < neparniBrojac; i++)
+            {
+                Console.WriteLine(neparniNiz[i]);
+            }
+
+            Console.WriteLine("Parni brojevi:");
+            for (int i = 0; i < parniBrojac; i++)
+            {
+                Console.WriteLine(parniNiz[i]);
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
